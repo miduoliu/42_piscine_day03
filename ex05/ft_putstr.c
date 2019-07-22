e017d7ffbbd5cffb6c00a556e726f6d176a4584a
Steven Liu <52724724+miduoliu@users.noreply.github.com>
2019-07-21 20:22:21 -0700

    Add files via upload

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miliu <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 22:27:37 by miliu             #+#    #+#             */
+/*   Updated: 2019/07/15 10:40:26 by miliu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+void	ft_putstr(char *str)
+{
+	int index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		ft_putchar(str[index]);
+		index++;
+	}
+}
+
+int	main(void)
+{
+	char *str;
+
+	*str = "abcde";
+	ft_putstr(*str);
+	return(0);
+}
